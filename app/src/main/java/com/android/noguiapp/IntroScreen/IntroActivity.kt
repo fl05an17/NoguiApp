180a013a8a875854008e53d28503e34ce7dc1786
fl05an17 <31561075+fl05an17@users.noreply.github.com>
2019-10-24 23:31:22 -0500

    Sprint with the following activities
    1.Design activity profile user

--- app/src/main/java/com/android/noguiapp/IntroScreen/IntroActivity.kt
+++ app/src/main/java/com/android/noguiapp/IntroScreen/IntroActivity.kt
@@ -1,8 +1,18 @@
 package com.android.noguiapp.IntroScreen
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import androidx.viewpager.widget.ViewPager
+import com.android.noguiapp.ActivityRegister
+import com.android.noguiapp.MainActivity
 import com.android.noguiapp.R
 import com.google.android.material.tabs.TabLayout
 
@@ -11,6 +21,10 @@ class IntroActivity : AppCompatActivity() {
     private var screenPager: ViewPager? = null
     protected lateinit var introViewPagerAdapter: IntroViewPagerAdapter
     protected lateinit var tabIndicator: TabLayout
+    protected lateinit var btnNext: Button
+    protected var position = 0
+    protected lateinit var btnGetStart: Button
+    protected lateinit var btnAnim: Animation
     private lateinit var title1: String
     private lateinit var title2: String
     private lateinit var title3: String
@@ -19,9 +33,31 @@ class IntroActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        //ini views
+        //make the activity on full screen
+        /*requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )*/
 
+        //when this activity is about to be launch we need to check if its opened before or not
+
+        if (restorePrefData()) {
+            val mainActivity = Intent(applicationContext, MainActivity::class.java)
+            startActivity(mainActivity)
+            finish()
+        }
+
+        setContentView(R.layout.activity_intro)
+
+        //hide the action bar
+        /*supportActionBar!!.hide()*/
+
+        //ini views
+        btnNext = findViewById(R.id.btnNext)
+        btnGetStart = findViewById(R.id.btn_get_start)
         tabIndicator = findViewById(R.id.tab_indicator)
+        btnAnim = AnimationUtils.loadAnimation(applicationContext, R.anim.button_animation)
 
         title1 = (resources.getString(R.string.title_intro_1))
         title2 = (resources.getString(R.string.title_intro_2))
@@ -53,7 +89,73 @@ class IntroActivity : AppCompatActivity() {
         screenPager!!.adapter = introViewPagerAdapter
 
         //setup tablayout with viewpager
-
         tabIndicator.setupWithViewPager(screenPager)
+
+        //next button click listener
+        btnNext.setOnClickListener {
+            position = screenPager!!.currentItem
+            if (position < mList.size - 1) {
+                position++
+                screenPager!!.currentItem = position
+            }
+            if (position == mList.size) {//when we rench to the last screen
+                //TODO : show the GETSTARTED Button and hide the indicator and the next button
+                loadLastScreen()
+            }
+        }
+
+        //tablayout add change listener
+        tabIndicator.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                if (tab.position == mList.size - 1) {
+                    loadLastScreen()
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+        })
+
+        //get started button click listener
+        btnGetStart.setOnClickListener {
+            //open main activity
+            val mainActivity = Intent(applicationContext, ActivityRegister::class.java)
+            startActivity(mainActivity)
+
+            /*also we need to save a boolean value to storage so next time when the
+                user run the app we could now that he is already checked the intro screen activity */
+
+            savePrefsData()
+            finish()
+        }
+
+    }
+
+    private fun restorePrefData(): Boolean {
+        val pref = applicationContext.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        return pref.getBoolean("isIntroOpened", false)
+    }
+
+    private fun savePrefsData() {
+        val pref = applicationContext.getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        editor.putBoolean("isIntroOpened", true)
+        editor.commit()
+    }
+
+    //show the GETSTARTED Button and hide the indicator and the next button
+    private fun loadLastScreen() {
+        btnNext.visibility = View.INVISIBLE
+        btnGetStart.visibility = View.VISIBLE
+        tabIndicator.visibility = View.INVISIBLE
+        //TODO : ADD an animation the getstarted button
+        //setup animation
+        btnGetStart.animation = btnAnim
+
     }
 }
